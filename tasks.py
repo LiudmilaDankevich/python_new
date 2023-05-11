5dcc6a6a380d80887686954f3ca2597b95a45499
LiudmilaDankevich <Dankevich_79@mail.ru>
2023-05-11 14:04:46 +0300

    tasks

---
+++ tasks.py
@@ -0,0 +1,28 @@
+import random
+import math
+
+x = s = 12
+y = 6
+# print("x:", x, "s:", s)
+
+# print(type(3.5), 3.5)
+# print(type(float(3)), float(3))
+# # Округление до 4 знаков после запятой.
+# print(round(10/3.0, 4))
+# print(abs(-5))
+# print(pow (3, 3))
+# print(random.random())
+# print(random.choice([1, 2, 3, 7]))
+# print((random.randrange(99)))
+# print(random.randrange(1, 2, 3))
+print("Корень числа 9:", math.sqrt(9))
+print(math.atan2(1, 1))
+print(math.factorial(3))
+# Конкатенация
+S1 = "first"
+S2 = "second"
+print(S1 + S2)
+
+
+
+
